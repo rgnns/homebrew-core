423de6401f1b4409c7705e4086fce3e51c626da0
Gabriel Lievano <jglievano@gmail.com>
2020-05-19 11:14:08 -0700

    Pull icon from repo.

--- Formula/emacs-for-macos.rb
+++ Formula/emacs-for-macos.rb
@@ -24,7 +24,7 @@ class EmacsForMacos < Formula
   depends_on "harfbuzz" => :recommended
 
   resource "modern-icon-sexy-v2" do
-    url "https://raw.githubusercontent.com/daviderestivo/homebrew-emacs-head/master/icons/modern-icon-sexy-v2.icns"
+    url "https://raw.githubusercontent.com/jglievano/homebrew-emacs-for-macos/master/icons/modern-icon-sexy-v2.icns"
     sha256 "ecdc902435a8852d47e2c682810146e81f5ad72ee3d0c373c936eb4c1e0966e6"
   end
 
